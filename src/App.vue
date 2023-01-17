38b401787f6d149e60ecb47079f6492881e8cac4
intervinn <69168328+intervinn@users.noreply.github.com>
2023-01-17 20:23:08 +0300

    Deploy config

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <Header/>
+  <Info/>
 </template>
 
 <script>
 
 import Header from './components/Header.vue';
+import Info from './components/Info.vue';
 
 export default {
   components: {
-    Header
+    Header,
+    Info
   }
 }
 </script>
---
+++ src/components/Info.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        наш айпи 81.176.176.58:25565
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
