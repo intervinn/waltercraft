58248aa101457e9a18abb049d0ad7658bab09f6a
intervinn <69168328+intervinn@users.noreply.github.com>
2024-04-29 10:29:59 +0300

    empty

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import Aos from 'aos'
-import { createApp } from 'vue'
-import App from './App.vue'
-
-import "aos/dist/aos.css"
-import "./styles/index.css"
-
-createApp(App).mount('#app')
-Aos.init()
